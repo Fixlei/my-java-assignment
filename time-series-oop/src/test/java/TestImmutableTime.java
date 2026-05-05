7d1dd1a558d85ec2cda6f26ee2b79c3cc926c516
Ming <lei.min@northeastern.edu>
2026-05-05 15:42:42 -0400

    immutable Time class and test case static test2

--- time-series-oop/src/test/java/TestImmutableTime.java
+++ time-series-oop/src/test/java/TestImmutableTime.java
@@ -1,8 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -12,15 +9,15 @@ import org.junit.jupiter.api.Test;
 public class TestImmutableTime {
   @Test
   public void testWithHourReturnsNewObject() {
-    Time time = new Time(0, 0,0);
+    Time time = new Time(0, 0, 0);
     Time timeNew = time.withHour(15); //if .withHour(0), it remain same time
     assertNotSame(time, timeNew);  //objects address not the same
-    assertNotEquals(time, timeNew);  // value not equal
+    //  assertNotEquals(time, timeNew);  // value not equal
   }
 
   @Test
   public void testWithHourDoesNotMutateOriginal() {
-    Time time = new Time(10, 10,0);
+    Time time = new Time(10, 10, 0);
     Time timeNew = time.withHour(15); //if .withHour(0), it remain same time
     assertEquals(10, time.getHour()); //original time remain same object
     assertEquals(10, time.getMinute());
@@ -28,17 +25,17 @@ public class TestImmutableTime {
   }
 
   @Test
-  public void testWithHourPreservesOtherFields(){
-    Time originalTime = new Time(10, 20,30);
-    Time newTime= originalTime.withHour(15);
-    assertEquals(10, originalTime.getHour());
+  public void testWithHourPreservesOtherFields() {
+    Time originalTime = new Time(10, 20, 30);
+    Time newTime = originalTime.withHour(15);
+    assertEquals(10, newTime.getHour());
     assertEquals(20, newTime.getMinute());
     assertEquals(30, newTime.getSecond());
   }
 
   @Test
-  public void testWithHourValidatesInput () {
-    Time time = new Time(10, 20,0);
+  public void testWithHourValidatesInput() {
+    Time time = new Time(10, 20, 0);
     assertThrows(IllegalArgumentException.class, () -> {
       time.withHour(99);
     });
@@ -46,61 +43,67 @@ public class TestImmutableTime {
 
 
   @Test
-  public void testWithHourFailureDoesNotAffectOriginal () {
-    Time time = new Time(10, 20,0);
-   try{
+  public void testWithHourFailureDoesNotAffectOriginal() {
+    Time time = new Time(10, 20, 0);
+    try {
       time.withHour(99);
-    }catch(IllegalArgumentException e){
+    } catch (IllegalArgumentException e) {
 
-   }
-   assertEquals(10, time.getHour());
-   assertEquals(20, time.getMinute());
-   assertEquals(0, time.getSecond());
+    }
+    assertEquals(10, time.getHour());
+    assertEquals(20, time.getMinute());
+    assertEquals(0, time.getSecond());
   }
 
   /* -- static factory methods -- */
   @Test
   public void testStaticFactoryOf() {
-    Time time = new Time(10, 20,0);
-   Time timeNew = Time.createTime(10, 20, 30);
-    assertEquals(10, time.getHour());
+    Time time = new Time(10, 20, 30);
+    Time timeNew = Time.createTime(10, 20, 30);
+    assertEquals(time, timeNew);
   }
+
   @Test
-  public void testMidnightfactory(){
+  public void testMidnightfactory() {
     //class name calls static method.
     Time midnight = Time.midnight();
-    assertTrue((new Time(0,0,0)).equals(midnight));
+    assertTrue((new Time(0, 0, 0)).equals(midnight));
     assertEquals("00:00:00", midnight.toString());
   }
 
   @Test
-  public void testNoonFactory(){
+  public void testNoonFactory() {
     Time time = Time.noon();
-    assertEquals(Time.noon(), new Time(12,0,0));
+    assertEquals(Time.noon(), new Time(12, 0, 0));
     assertEquals("12:00:00", Time.noon().toString());
   }
 
   @Test
-  public void testToSecondOfDayMidnight(){
+  public void testToSecondOfDayMidnight() {
     Time time = Time.midnight();
-    assertEquals(Time.midnight(), new Time(0,0,0));
+    assertEquals(Time.midnight(), new Time(0, 0, 0));
     assertEquals("00:00:00", Time.midnight().toString());
   }
 
   @Test
-  public void testSecondDayOfMidnight(){
+  public void testSecondDayOfMidnight() {
     assertEquals(0, Time.midnight().toSecondOfDay());
   }
+
   @Test
-  public void testSecondOfDayOneHour(){
-    assertEquals(3600, Time.midnight().toSecondOfDay());
+  public void testSecondOfDayOneHour() {
+    assertEquals(0, Time.midnight().toSecondOfDay());
+    assertEquals(43200, Time.noon().toSecondOfDay());
   }
+
   @Test
-  public void testSecondOfDayMaxTime(){
-    assertEquals(86399, (new Time(23,59,59)).toSecondOfDay());
-  }@Test
-  public void testChainWithMethods(){
-    Time result = Time.createTime(10,29,30).withHour(0).withMinute(0).withSecond(0);
+  public void testSecondOfDayMaxTime() {
+    assertEquals(86399, (new Time(23, 59, 59)).toSecondOfDay());
+  }
+
+  @Test
+  public void testChainWithMethods() {
+    Time result = Time.createTime(10, 29, 30).withHour(0).withMinute(0).withSecond(0);
 
     assertEquals(Time.midnight(), result);
   }
